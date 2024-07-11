a9504dd7a842b950246677cac2a1387e22763d42
boyani-Otieno Sylvia <boyani.ombogo7@gmail.com>
2024-07-11 14:14:29 +0000

    edit team section

--- src/components/home/hero/Hero.jsx
+++ src/components/home/hero/Hero.jsx
@@ -1,39 +1,60 @@
-import React from "react"
+import React from "react";
 import { GiInspiration } from "react-icons/gi";
-import Heading from "../../common/Heading"
-import "./hero.css"
+import Heading from "../../common/Heading";
+import { motion } from "framer-motion";
+import "./hero.css";
 
 const Hero = () => {
+  const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        delayChildren: 0.3,
+        staggerChildren: 0.2,
+      },
+    },
+  };
+
+  const textVariants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+  };
+
   return (
-    <>
-      <section className='hero'>
-        <div className='container'>
+    <section className='hero'>
+      <div className='container'>
+        <motion.div
+          initial="hidden"
+          animate="visible"
+          variants={containerVariants}
+        >
           <Heading title='Join the cause ' subtitle='Make a difference, No matter how small.' />
 
           <form className='flex'>
-            <div className='box'>
+            <motion.div className='box' variants={textVariants}>
               <span>Serve</span>
               <input type='text' placeholder='Anywhere' />
-            </div>
-            <div className='box'>
+            </motion.div>
+            <motion.div className='box' variants={textVariants}>
               <span>Inspire</span>
               <input type='text' placeholder='Young ones' />
-            </div>
-            <div className='box'>
+            </motion.div>
+            <motion.div className='box' variants={textVariants}>
               <span>Teach</span>
               <input type='text' placeholder='Everyone' />
-            </div>
-            <div className='box'>
+            </motion.div>
+            <motion.div className='box' variants={textVariants}>
               <h4>Protect</h4>
-            </div>
-            <button className='btn1'>
-            <GiInspiration />
-            </button>
+            </motion.div>
+            <motion.button className='btn1' variants={textVariants}>
+              <GiInspiration />
+            </motion.button>
           </form>
-        </div>
-      </section>
-    </>
-  )
-}
+        </motion.div>
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
--- src/components/home/team/Team.jsx
+++ src/components/home/team/Team.jsx
@@ -1,46 +1,86 @@
-import React from "react"
-import Heading from "../../common/Heading"
-import { team } from "../../data/Data"
-import "./team.css"
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import './team.css';
+import img from '../../../assets/images/customer/jeda.jpeg';
+import im3 from '../../../assets/images/customer/boke.jpeg';
+import im4 from '../../../assets/images/customer/prudence.jpeg';
 
-const Team = () => {
+function Team() {
+  useEffect(() => {
+    AOS.init({
+      duration: 2000,
+      once: true,
+    });
+  }, []);
+
   return (
-    <>
-      <section className='team background'>
-        <div className='container'>
-          <Heading title='Meet Our Team' subtitle='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.' />
-
-          <div className='content mtop grid3'>
-            {team.map((val, index) => (
-              <div className='box' key={index}>
-                <div className='details'>
-                  <div className='img'>
-                    <img src={val.cover} alt='' />
-                  </div>
-                  <h4>{val.name}</h4>
-
-                  <ul>
-                    {val.icon.map((icon, index) => (
-                      <li key={index}>{icon}</li>
-                    ))}
-                  </ul>
-                  <div className='button flex'>
-                    {/* <button>
-                      <i className='fa fa-envelope'></i>
-                      Message
-                    </button>
-                    <button className='btn4'>
-                      <i className='fa fa-phone-alt'></i>
-                    </button> */}
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
+    <div className='me'>
+      <div data-aos="fade-right" className="about-column">
+        <div className='about-img'>
+          <img src={img} alt="about us" />
         </div>
-      </section>
-    </>
-  )
+      </div>
+
+      <div data-aos="fade-left" className="about-column">
+        <div className='about-content'>
+          <h3>Jeridah Gati</h3>
+          <h3>CEO and Founder</h3>
+          <p>We all have our strengths. I’ve perfected
+          mine <br />so you can focus on yours. Get in touch
+          to find <br />out how I can help.
+          Because I'm experienced, reliable,
+          and focused on results, I've had the
+          privilege of working with a host of fantastic clients.
+          </p>
+        </div>
+      </div>
+
+      <div data-aos="fade-left" className="about-column">
+        <div className='about-content'>
+          <p><h3>Martha Boke</h3>
+          <h3>Co-Founder</h3>
+          My business is more than just a job.
+          Each time I take on a new project,
+          I make sure it aligns with my core
+          set of values so I know I can deliver great results.
+          My curiosity makes me love learning, and added with my
+          perfectionism, I am able to come up with quality work.
+          Because I'm experienced, reliable,
+          and focused on results, I've had the
+          privilege of working with a host of fantastic clients.
+          </p>
+        </div>
+      </div>
+
+      <div data-aos="fade-right" className="about-column">
+        <div className='about-img'>
+          <img src={im3} alt="about us" />
+        </div>
+      </div>
+
+      <div data-aos="fade-right" className="about-column">
+        <div className='about-img-prudence'>
+          <img src={im4} alt="about us" />
+        </div>
+      </div>
+
+      <div data-aos="fade-left" className="about-column">
+        <div className='about-content'>
+          <p>
+          <h3>Prudence Prudence</h3>
+          <h3>Co-Founder</h3>
+          Because I'm experienced, reliable,
+          and focused on results, I've had the
+          privilege of working with a host of fantastic clients.
+          Because I'm experienced, reliable,
+          and focused on results, I've had the
+          privilege of working with a host of fantastic clients.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Team
+export default Team;
