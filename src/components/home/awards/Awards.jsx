a39240960371b79ffb4f511308fca6be5a62f129
boyani-Otieno Sylvia <boyani.ombogo7@gmail.com>
2024-09-09 09:46:57 +0000

    add maps section

--- src/components/home/awards/Awards.jsx
+++ src/components/home/awards/Awards.jsx
@@ -1,30 +1,46 @@
-import React from "react"
-import Heading from "../../common/Heading"
-import { awards } from "../../data/Data"
-import "./awards.css"
+import React from "react";
+import "./awards.css"; // Make sure to create and customize this CSS file
+import { FaUserMd, FaBaby, FaWheelchair } from "react-icons/fa"; // Example icons
 
-const Awards = () => {
+const AboutUs = () => {
   return (
-    <>
-      <section className='awards padding'>
-        <div className='container'>
-          <Heading title='Impressive Achievements: Noteworthy Awards Earned on Our Journey' subtitle='Our Awards' />
+    <section className="about-us">
+      <div className="about-us-content">
+        <div className="about-us-text">
+          <h2>WHO WE ARE</h2>
+          <hr className="about-us-divider" />
+          <p>
+            We work tirelessly to improve the lives of vulnerable communities in
+            Kenya. Our special missions include healthcare, education, and
+            sustainable development projects across the country.
+          </p>
+        </div>
 
-          <div className='content grid4 mtop'>
-            {awards.map((val, index) => (
-              <div className='box' key={index}>
-                <div className='icon'>
-                  <span>{val.icon}</span>
-                </div>
-                <h1>{val.num}</h1>
-                <p>{val.name}</p>
-              </div>
-            ))}
+        <div className="about-us-stats">
+          <div className="stat-item">
+            <FaUserMd className="stat-icon" />
+            <h3>52,000</h3>
+            <p>Women and children we've screened & treated for anemia</p>
+          </div>
+
+          <div className="stat-item">
+            <FaBaby className="stat-icon" />
+            <h3>34,000</h3>
+            <p>Women we've helped deliver healthy babies</p>
+          </div>
+
+          <div className="stat-item">
+            <FaWheelchair className="stat-icon" />
+            <h3>820</h3>
+            <p>
+              Received free hip & knee replacements, bone fracture repairs, or
+              minimally-invasive surgery
+            </p>
           </div>
         </div>
-      </section>
-    </>
-  )
-}
+      </div>
+    </section>
+  );
+};
 
-export default Awards
+export default AboutUs;
