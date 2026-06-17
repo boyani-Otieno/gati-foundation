62fa32218d449c6a587de661838325164c880a9e
boyani-Otieno <152180850+boyani-Otieno@users.noreply.github.com>
2026-06-17 19:04:55 +0000

    Edit the footer

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -1,72 +1,116 @@
-import React from 'react';
+import React from "react";
 import { FaFacebookSquare } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import { FaInstagramSquare } from "react-icons/fa";
-import './footer.css';
+import "./footer.css";
 
 const Footer = () => {
   return (
     <footer className="footer">
-      {/* <div className="footer-logo">
-        <img src="/path/to/logo.png" alt="SHOFCO Logo" />
-        <p>Building Promise from Poverty</p>
-      </div> */}
 
-      <div className="footer-links">
+      <div className="footer-container">
+
+        <div className="footer-brand">
+
+          <h2>
+            Margaret Robi Foundation
+          </h2>
+
+          <p>
+            Empowering women and girls through education, leadership,
+            advocacy, and the fight against harmful cultural practices.
+          </p>
+
+          <div className="footer-social">
+
+            <a href="https://www.facebook.com" target="_blank" rel="noreferrer">
+              <FaFacebookSquare />
+            </a>
+
+            <a href="https://www.twitter.com" target="_blank" rel="noreferrer">
+              <FaXTwitter />
+            </a>
+
+            <a href="https://www.instagram.com" target="_blank" rel="noreferrer">
+              <FaInstagramSquare />
+            </a>
+
+          </div>
+
+        </div>
+
+
+
         <div className="footer-section">
+
           <h4>About Us</h4>
+
           <ul>
             <li><a href="/our-story">Our Story</a></li>
             <li><a href="/our-people">Our People</a></li>
-            <li><a href="/awards">Awards</a></li>
             <li><a href="/partners">Partners</a></li>
+            <li><a href="/awards">Awards</a></li>
           </ul>
+
         </div>
 
+
+
         <div className="footer-section">
-          <h4>Impact</h4>
+
+          <h4>Our Impact</h4>
+
           <ul>
+            <li><a href="/programs">Programs</a></li>
             <li><a href="/the-need">The Need</a></li>
-            <li><a href="/shifting-paradigm">Shifting Paradigm</a></li>
-            <li><a href="/monitoring-evaluation">Monitoring & Evaluation</a></li>
+            <li><a href="/monitoring">Monitoring & Evaluation</a></li>
+            <li><a href="/stories">Stories</a></li>
           </ul>
+
         </div>
+
+
 
         <div className="footer-section">
-          <h4>Support Us</h4>
-          <ul>
-            <li><a href="/donate">Donate</a></li>
-            <li><a href="/other-ways-to-give">Other Ways to Give</a></li>
-            <li><a href="/sponsorship">Sponsorship</a></li>
-          </ul>
+
+          <h4>Contact</h4>
+
+          <p>Email:</p>
+          <span>
+            margaretrobifoundation@gmail.com
+          </span>
+
+
+          <p>Phone:</p>
+          <span>
+            +254 727761064
+          </span>
+
+
+          <button>
+            Donate Now
+          </button>
+
+
         </div>
 
-        <div className="footer-section">
-          <h4>Contact Us</h4>
-          <p>Email: margaretrobifoundation@gmail.com</p>
-          <p>Phone: +254 727761064</p>
-        </div>
+
       </div>
 
-      <div className="footer-social">
-        <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer"><FaFacebookSquare/></a>
-        <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer"><FaXTwitter/></a>
-        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer"><FaInstagramSquare/></a>
+
+
+      <div className="footer-bottom">
+
+        <p>
+          © 2026 Margaret Robi Foundation. All Rights Reserved.
+        </p>
+
       </div>
 
-      {/* <div className="footer-newsletter">
-        <h4>Join our Newsletter</h4>
-        <form>
-          <input type="email" placeholder="Your Email" />
-          <button type="submit">Subscribe</button>
-        </form>
-      </div> */}
 
-      <div className="footer-copyright">
-        <p>© 2024 MARGARET ROBI FOUNDATION. All rights reserved.</p>
-      </div>
     </footer>
   );
 };
+
 
 export default Footer;
