dd4613c9bcb2d912b38d106fffa1f62a1935d7e5
boyani-Otieno <152180850+boyani-Otieno@users.noreply.github.com>
2026-07-17 16:49:36 +0000

    add socials links

--- src/components/common/header/SmallNav.jsx
+++ src/components/common/header/SmallNav.jsx
@@ -9,18 +9,18 @@ const SmallNavbar = () => {
         <p>Contact us: +254 727761064 | margaretrobifoundation@gmail.com</p>
       </div>
       <div className="social-media-icons">
-        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.facebook.com/share/18yUpuGDjq/?mibextid=wwXIfrm" target="_blank" rel="noopener noreferrer">
           <FaFacebookF />
         </a>
-        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+        <a href="https://x.com/MargaretRobiFdn" target="_blank" rel="noopener noreferrer">
           <FaTwitter />
         </a>
-        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.instagram.com/margaret_robi_foundation?igsh=enBmanQxN3M5ZjVh&utm_source=qr" target="_blank" rel="noopener noreferrer">
           <FaInstagram />
         </a>
-        <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.linkedin.com/company/margaret-robi-foundation/" target="_blank" rel="noopener noreferrer">
           <FaLinkedinIn />
-        </a>
+        </a> 
       </div>
     </div>
   );
